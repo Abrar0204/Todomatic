e00b9c192ad59968d2177e4389cd7c77d5e43722
Abrar Hasan <abrarhasan0204@gmail.com>
2022-02-26 21:44:50 +0530

    added edit feature

--- src/components/TodoItem/TodoItem.js
+++ src/components/TodoItem/TodoItem.js
@@ -1,6 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 
 const TodoItem = ({ todo, todoIndex, setTodos }) => {
+  const [isEditing, setIsEditing] = useState(false);
+  const [editTodoInput, setEditTodoInput] = useState("");
+
   const deleteTodo = () => {
     setTodos((prevTodos) =>
       prevTodos.filter((_, index) => todoIndex !== index)
@@ -20,18 +23,51 @@ const TodoItem = ({ todo, todoIndex, setTodos }) => {
     );
   };
 
+  const toggleEdit = () => {
+    setEditTodoInput(todo.text);
+    setIsEditing(true);
+  };
+
+  const saveEdit = () => {
+    setTodos((prevTodos) =>
+      prevTodos.map((todo, index) => {
+        if (todoIndex === index) {
+          return { ...todo, text: editTodoInput };
+        } else {
+          return todo;
+        }
+      })
+    );
+    setIsEditing(false);
+  };
+
+  const handleInput = (event) => {
+    const { value } = event.target;
+    setEditTodoInput(value);
+  };
+
   return (
     <li>
-      <label>
-        <input
-          type="checkbox"
-          deafultChecked={todo.isCompleted}
-          onChange={checkOrUncheckTodo}
-        />
-        {todo.text}
-      </label>
+      {isEditing ? (
+        <input value={editTodoInput} onChange={handleInput} />
+      ) : (
+        <label>
+          <input
+            type="checkbox"
+            deafultChecked={todo.isCompleted}
+            onChange={checkOrUncheckTodo}
+          />
+          {todo.text}
+        </label>
+      )}
       <br />
       <button onClick={deleteTodo}>Delete Todo</button>
+      <br />
+      {isEditing ? (
+        <button onClick={saveEdit}>Save Edit</button>
+      ) : (
+        <button onClick={toggleEdit}>Edit Todo</button>
+      )}
     </li>
   );
 };
